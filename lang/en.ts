bfb239bde57880a918587ca97a81f63ebbd44e90
Martin Binder <72764328+bndrmrtn@users.noreply.github.com>
2024-08-16 15:46:16 +0200

    translate

--- lang/en.ts
+++ lang/en.ts
@@ -13,4 +13,10 @@ export default {
     'Személyzet': 'Staffs',
     'Steam ID Másolása': 'Copy Steam ID',
     'Steam profil': 'Steam profile',
+
+    // Computer needs table
+    '4 hardware CPU threads - Intel® Core™ i5-750 or higher': '4 hardware CPU threads - Intel® Core™ i5-750 or higher',
+    'Video card must be 1 GB or more and should be a DirectX 11-compatible with support for Shader Model 5.0': 'Video card must be 1 GB or more and should be a DirectX 11-compatible with support for Shader Model 5.0',
+    'Version 11': 'Version 11',
+    '85 GB available space': '85 GB available space',
 }
--- lang/hu.ts
+++ lang/hu.ts
@@ -1,3 +1,7 @@
 export default {
-
+    // Computer needs table
+    '4 hardware CPU threads - Intel® Core™ i5-750 or higher': '4 hardveres CPU-szál - Intel® Core™ i5-750 vagy annál magasabb',
+    'Video card must be 1 GB or more and should be a DirectX 11-compatible with support for Shader Model 5.0': 'A videokártyának legalább 1 GB-osnak kell lennie, és DirectX 11-kompatibilisnek, a Shader Model 5.0 támogatásával.',
+    'Version 11': '11-es verzió',
+    '85 GB available space': '85 GB rendelkezésre álló hely',
 }
